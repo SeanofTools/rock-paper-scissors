327dd256ca2040e11f04ec11df407b855b7b0a72
Sean Javier <sean.javier0@gmail.com>
2019-09-11 13:28:57 +0900

    highlight click

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,30 @@
         let userInput;
         //user click will determine what gets returned
-        const userRock = () => userInput = 'rock'
-        const userPaper = () => userInput = 'paper'
-        const userScissors = () => userInput = 'scissors'
-        const userCheat = () => userInput = 'gun'
+        const userRock = () => {
+		document.getElementById('rock').style.opacity = 1;
+		document.getElementById('paper').style.opacity = 0.4;
+		document.getElementById('scissors').style.opacity = 0.4;
+		userInput = 'rock';
+	 }
+        const userPaper = () => {
+		document.getElementById('rock').style.opacity = 0.4;
+		document.getElementById('paper').style.opacity = 1;
+		document.getElementById('scissors').style.opacity = 0.4;
+		userInput = 'paper';
+	 }
+        const userScissors = () => {
+		document.getElementById('rock').style.opacity = 0.4;
+		document.getElementById('paper').style.opacity = 0.4;
+		document.getElementById('scissors').style.opacity = 1;
+		userInput = 'scissors';
+	 }
+        const userCheat = () => {
+		document.getElementById('rock').style.opacity = 1;
+		document.getElementById('paper').style.opacity = 1;
+		document.getElementById('scissors').style.opacity = 1;
+		userInput = 'gun'
+	}
+
 	 //declare scores
 	 var userScore = 0;
 	 var cpuScore = 0;
@@ -29,7 +50,7 @@
                 document.getElementById("compResult").innerHTML = 'You used '+ user.toUpperCase() +' and the CPU used '+ cpMove.toUpperCase() +'. It\s a draw!';
 			} else if (user === 'rock' && cpMove === 'paper') {
                 document.getElementById("compResult").innerHTML = 'You used ROCK and the CPU used PAPER. The CPU wins!';
-			cpuScore++;
+					cpuScore++;
 			} else if (user === 'paper' && cpMove === 'scissors') {
                 document.getElementById("compResult").innerHTML = 'You used PAPER and the CPU used SCISSORS. The CPU wins!';
 			cpuScore++;
